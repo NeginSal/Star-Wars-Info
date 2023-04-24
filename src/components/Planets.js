ed358a2a686e8de56a8f1a7e52b460a419fec9c3
NeginSal <neginsaljooghi@gmail.com>
2023-04-24 11:36:36 +0330

    added QueryClient

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useQuery } from 'react-query'
 
 const fetchPlanet = async () => {
-    const res = await fetch('http://swapi.dev/api/planets/')
+    const res = await fetch('http://swapi.dev/api/planets/');
     return res.json()
-
 }
 
 const Planet = () => {
+    
     const { data, status } = useQuery('planets', fetchPlanet)
+    console.log(data)
+
     return (
         <div>
             {status === 'loading' && (<div>Loading ...</div>)}
             {status === 'error' && (<div>Error</div>)}
         </div>
+
     );
 }
 
