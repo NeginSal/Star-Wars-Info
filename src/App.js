93c67b56e653318e2470681907f53eef543ede76
NeginSal <neginsaljooghi@gmail.com>
2023-04-29 10:28:11 +0330

    added react-query devtools

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Navbar from "./components/Navbar";
-import Planets from "./components/Planets";
 import People from "./components/People";
+import Planets from "./components/Planets";
+// import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient();
 
@@ -10,15 +12,20 @@ function App() {
 
   const [page, setPage] = useState('planets')
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Star Wars Info</h1>
-        <Navbar setPage={setPage} />
-        <div className="content">
-          {page === 'planets' ? <Planets /> : <People />}
+    
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>Star Wars Info</h1>
+          <Navbar setPage={setPage} />
+          <div className="content">
+            {page === 'planets' ? <Planets /> : <People />}
+          </div>
         </div>
-      </div>
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+
+    
+
   );
 }
 
