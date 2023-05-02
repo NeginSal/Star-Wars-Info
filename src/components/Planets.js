92c75cd595f162d30e7aa1c83cd3b5ea31ab583d
NeginSal <neginsaljooghi@gmail.com>
2023-05-02 11:43:22 +0330

    added pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -3,22 +3,22 @@ import { useQuery } from 'react-query';
 import Planet from './Planet';
 
 const fetchPlanets = async (page) => {
-    const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
-    return res.json();
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
+  return res.json();
 };
 
 const Planets = () => {
-  const [ page, setPage ] = useState(1);
-  const {data, status} = useQuery(['planets', page], () => fetchPlanets(page));
-  console.log(data, status )
+  const [page, setPage] = useState(1);
+  const { data, status, isPreviousData } = useQuery({
+    queryKey: ['planets', page],
+    queryFn: () => fetchPlanets(page),
+  })
+
+  //useQuery(['planets', page], () => fetchPlanets(page), keepPreviousData : true);
+  console.log(data, status)
   return (
     <div>
       <h2>Planets</h2>
-
-      <button onClick={() => setPage(1)}>page 1</button>
-      <button onClick={() => setPage(2)}>page 2</button>
-      <button onClick={() => setPage(3)}>page 3</button>
-
       {status === 'loading' && (
         <div>Loading data</div>
       )}
@@ -28,10 +28,28 @@ const Planets = () => {
       )}
 
       {status === 'success' && (
-        <div>
-          { data.results.map(planet => <Planet key={planet.name} planet={planet} /> ) }
-        </div>
-      )} 
+        <>
+          <button
+            onClick={() => setPage(old => Math.max(old - 1, 1))}
+            disabled={page === 1}>Previous Page</button>
+          <span>{page}</span>
+          <button
+            onClick={() => {
+              if (!isPreviousData && data.next) {
+                setPage(old => old + 1)
+              }
+            }}
+            // Disable the Next Page button until we know a next page is available
+            disabled={isPreviousData || !data?.next}
+          >
+            Next Page
+          </button>
+          <div>
+            {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+          </div>
+
+        </>
+      )}
     </div>
   );
 }
